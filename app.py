68f0c2ae1a4ca5bc1c814f8faf59f0b8ad3ccb8d
Haoli <haoli.yin@vanderbilt.edu>
2023-07-13 13:57:35 -0500

    Add functional frontend

--- app.py
+++ app.py
@@ -2,18 +2,17 @@
 import streamlit as st
 import plotly.express as px
 import pandas as pd
-import numpy as np
+import psycopg2
 
-# Load your data (placeholder until your database is ready)
-# This will create a DataFrame with 1000 samples, each with a 3D point, a camid, and a pid.
+@st.cache(hash_funcs={psycopg2.extensions.connection: id})
+def get_connection():
+    return psycopg2.connect("dbname='postgres' user='postgres' host='localhost' password='hackathon' port='5434'")
+
 def load_data():
-    np.random.seed(0)
-    data = np.random.rand(1000, 3)  # Random 3D points
-    camids = np.random.randint(0, 5, 1000)  # Random camids
-    pids = np.random.randint(0, 5, 1000)  # Random pids
-    df = pd.DataFrame(data, columns=["x", "y", "z"])
-    df["camid"] = camids
-    df["pid"] = pids
+    conn = get_connection()
+    df = pd.read_sql('SELECT * FROM UMAPTable', conn)
+    df[['x', 'y', 'z']] = df[['x', 'y', 'z']].apply(pd.to_numeric)
+    df[['camid', 'pid']] = df[['camid', 'pid']].astype(int)
     return df
 
 df = load_data()
@@ -43,7 +42,7 @@ def main():
 
     # Create 3D scatter plot
     fig = px.scatter_3d(df_not_selected, x='x', y='y', z='z', color='pid',
-                        opacity=0.1, color_continuous_scale='RdBu', title="Embeddings")
+                        opacity=0.05, color_continuous_scale='RdBu', title="Embeddings")
     fig.update_layout(width=800, height=600)
 
     fig.add_trace(px.scatter_3d(df_selected, x='x', y='y', z='z', color='pid',
