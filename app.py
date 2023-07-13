05583ff11fc91c83d404342cf5f4461bf7ef0380
Haoli <haoli.yin@vanderbilt.edu>
2023-07-13 14:06:43 -0500

    Finish

--- app.py
+++ app.py
@@ -1,52 +1,47 @@
-# Import necessary libraries
 import streamlit as st
 import plotly.express as px
-import pandas as pd
 import psycopg2
+import pandas as pd
 
-@st.cache(hash_funcs={psycopg2.extensions.connection: id})
+@st.cache_resource(hash_funcs={psycopg2.extensions.connection: id})
 def get_connection():
     return psycopg2.connect("dbname='postgres' user='postgres' host='localhost' password='hackathon' port='5434'")
 
-def load_data():
+def load_data(selection_query):
     conn = get_connection()
-    df = pd.read_sql('SELECT * FROM UMAPTable', conn)
-    df[['x', 'y', 'z']] = df[['x', 'y', 'z']].apply(pd.to_numeric)
-    df[['camid', 'pid']] = df[['camid', 'pid']].astype(int)
+    df = pd.read_sql(selection_query, conn)
     return df
-
-df = load_data()
 
 # Streamlit app
 def main():
     st.title("3D Embeddings Visualization")
 
+    conn = get_connection()
+    cur = conn.cursor()
+    
+    cur.execute("SELECT DISTINCT camid FROM UMAPTable")
+    camid_options = [row[0] for row in cur.fetchall()]
+    cur.execute("SELECT DISTINCT pid FROM UMAPTable")
+    pid_options = [row[0] for row in cur.fetchall()]
+    
     # User input for camids and pids
-    camids = st.multiselect('Choose camids', df['camid'].unique())
-    pids = st.multiselect('Choose pids', df['pid'].unique())
+    camids = st.multiselect('Choose camids', camid_options)
+    pids = st.multiselect('Choose pids', pid_options)
 
-    # Modify the DataFrame based on user selection
+    selection_query = 'SELECT * FROM UMAPTable'
+    
     if camids:
-        df_selected_camid = df[df['camid'].isin(camids)]
-    else:
-        df_selected_camid = df
-
+        camids_str = ','.join([str(e) for e in camids])
+        selection_query += f' WHERE camid IN ({camids_str})'
+    
     if pids:
-        df_selected_pid = df[df['pid'].isin(pids)]
-    else:
-        df_selected_pid = df
-
-    df_selected = pd.merge(df_selected_camid, df_selected_pid)
-
-    df_not_selected = df.drop(df_selected.index)
+        pids_str = ','.join(["'" + str(e) + "'" for e in pids])  # Treat pids as strings
+        selection_query += (' AND' if 'WHERE' in selection_query else ' WHERE') + f' pid IN ({pids_str})'
+    
+    df = load_data(selection_query)
 
     # Create 3D scatter plot
-    fig = px.scatter_3d(df_not_selected, x='x', y='y', z='z', color='pid',
-                        opacity=0.05, color_continuous_scale='RdBu', title="Embeddings")
-    fig.update_layout(width=800, height=600)
-
-    fig.add_trace(px.scatter_3d(df_selected, x='x', y='y', z='z', color='pid',
-                        opacity=1, color_continuous_scale='RdBu').data[0])  # Add selected points with full opacity
+    fig = px.scatter_3d(df, x='x', y='y', z='z', color='pid', opacity=1, color_continuous_scale='RdBu', title="Embeddings")
     fig.update_layout(width=800, height=600)
 
     st.plotly_chart(fig)
